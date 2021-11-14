cd9355c8b6042106a97c6d77352eb5ce6d3aae0c
Valeriy Seregin <valera-s-38@mail.ru>
2021-11-14 15:46:24 +0300

    change layout links and add login test

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpacke = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpacke.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
