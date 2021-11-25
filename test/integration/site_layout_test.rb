87e1e22cdf9a5b20b5aa7f8a1b264f0223f93321
Valeriy Seregin <valera-s-38@mail.ru>
2021-11-25 22:11:01 +0300

    add users index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,11 +1,19 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:valeriy)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path, count: 0
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
